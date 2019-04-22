e3f507543987996ce3b8ced47f7ef6337995fa56
Alias M6 <aliasmontoya719@gmail.com>
2019-04-21 22:55:52 -0700

    refactor onSearchSubmit callback to an arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,25 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  state = { images: [] };
 
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {query: term},
       headers: {
         Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`
       }
-    }).then(res => {
-      console.log(res.data.results)
-    })
+    });
+
+    this.setState({ images: response.data.results });
+
   }
 
   render() {
     return (
       <div className="ui container" style={{marginTop:'10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     );
   }
