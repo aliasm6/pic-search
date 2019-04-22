52280fcbc612db3d83e7c45b65e2ef428da668fe
Alias M6 <aliasmontoya719@gmail.com>
2019-04-21 23:57:11 -0700

    improve accessibility and get rid of key issue

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const ImageList = (props) => {
- const images = props.images.map((image) => {
-   return <img key={image.id} alt={image.alt_description} src={image.urls.regular} />
+ const images = props.images.map(({id, alt_description, urls}) => {
+   return <img key={id} alt={alt_description} src={urls.regular} />
  })
  return <div>{images}</div>;
 }
